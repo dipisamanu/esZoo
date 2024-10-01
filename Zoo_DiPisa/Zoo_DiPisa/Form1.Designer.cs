12c4bc13c32339209248543f99c04f32782ffa9e
dipisa manu <145185168+dipisamanu@users.noreply.github.com>
2024-10-01 19:15:17 +0200

    es zoo 09 24
    file quasi funzionante ma con form al posto che console .net

---
+++ Zoo_DiPisa/Zoo_DiPisa/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace Zoo_DiPisa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(608, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(435, 85);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ZOO EDEN";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "",
+            "Pinguino",
+            "Rinoceronte",
+            "Giraffa",
+            "Scimmia",
+            "Serpente"});
+            this.comboBox1.Location = new System.Drawing.Point(97, 490);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(292, 39);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(541, 282);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 37);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nome";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(541, 374);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(276, 37);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Data Ultimo Pasto";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(541, 512);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(231, 37);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Caratteristica 1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(541, 584);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(233, 37);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Caratteristica 2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(541, 658);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(233, 37);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Caratteristica 3";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(90, 400);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(299, 37);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Seleziona la specie:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(937, 275);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(450, 44);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(937, 512);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(450, 44);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(937, 584);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(450, 44);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(937, 658);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(450, 44);
+            this.textBox5.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(655, 780);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(329, 67);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Aggiungi Animale";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(937, 374);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(450, 44);
+            this.dateTimePicker1.TabIndex = 14;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "",
+            "Pinguino",
+            "Rinoceronte",
+            "Giraffa",
+            "Scimmia",
+            "Serpente"});
+            this.comboBox2.Location = new System.Drawing.Point(2069, 780);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(292, 39);
+            this.comboBox2.TabIndex = 16;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(1663, 780);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(303, 37);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Specie da mostrare:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(1477, 292);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(1090, 450);
+            this.dataGridView1.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(2602, 996);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Zoo_DiPisa/Zoo_DiPisa/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zoo_DiPisa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<Pinguino> pinguini = new List<Pinguino>();
+        List<Rinoceronte> rinoceronti = new List<Rinoceronte>();
+        List<Giraffa> giraffe = new List<Giraffa>();
+        List<Scimmia> scimmie = new List<Scimmia>();
+        List<Serpente> serpenti = new List<Serpente>();
+        public void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine(comboBox1.SelectedIndex);
+            int selected = comboBox1.SelectedIndex;
+            switch (selected)
+            {
+                case 1:
+                    label4.Text = "Età";
+                    label5.Text = "Colore";
+                    label6.Text = "Peso";
+                    break;
+                case 2:
+                    label4.Text = "Età";
+                    label5.Text = "Corni";
+                    label6.Text = "Peso";
+                    break;
+                case 3:
+                    label4.Text = "Età";
+                    label5.Text = "Altezza";
+                    label6.Text = "Temperamento";
+                    break;
+                case 4:
+                    label4.Text = "Età";
+                    label5.Text = "Temperamento";
+                    label6.Text = "Velocità di Corsa";
+                    break;
+                case 5:
+                    label4.Text = "Età";
+                    label5.Text = "Lunghezza";
+                    label6.Text = "Colore";
+                    break;
+            }
+        }
+
+        public void TextboxClear()
+        {
+            textBox1.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox3.Text == "" || textBox4.Text == "" || textBox5.Text == "")
+            {
+                MessageBox.Show("Inserisci dati validi! ");
+                return;
+            }
+            int selected = comboBox1.SelectedIndex;
+            switch (selected)
+            {
+                case 1:
+                    Pinguino pinguino = new Pinguino(textBox1.Text, dateTimePicker1.Value, Convert.ToInt32(textBox3.Text), textBox4.Text, Convert.ToInt32(textBox5.Text));
+                    pinguini.Add(pinguino);
+                    TextboxClear();
+                    break;
+                case 2:
+                    Rinoceronte rinoceronte = new Rinoceronte(textBox1.Text, dateTimePicker1.Value, Convert.ToInt32(textBox3.Text), Convert.ToInt32(textBox4.Text), Convert.ToInt32(textBox5.Text));
+                    rinoceronti.Add(rinoceronte);
+                    TextboxClear();
+                    break;
+                case 3:
+                    Giraffa giraffa = new Giraffa(textBox1.Text, dateTimePicker1.Value, Convert.ToInt32(textBox3.Text), Convert.ToInt32(textBox4.Text), textBox5.Text);
+                    giraffe.Add(giraffa);
+                    TextboxClear();
+                    break;
+                case 4:
+                    Scimmia scimmia = new Scimmia(textBox1.Text, dateTimePicker1.Value, Convert.ToInt32(textBox3.Text), textBox4.Text, Convert.ToInt32(textBox5.Text));
+                    scimmie.Add(scimmia);
+                    TextboxClear();
+                    break;
+                case 5:
+                    Serpente serpente = new Serpente(textBox1.Text, dateTimePicker1.Value, Convert.ToInt32(textBox3.Text), Convert.ToInt32(textBox4.Text), textBox5.Text);
+                    serpenti.Add(serpente);
+                    TextboxClear();
+                    break;
+                default:
+                    MessageBox.Show("Selezione non valida");
+                    break;
+            }
+
+        }
+
+        private void PopolaDataGridView<T>(List<T> animali)
+        {
+            dataGridView1.DataSource = animali;
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selected = comboBox1.SelectedIndex;
+            switch (selected)
+            {
+                case 1:
+                    PopolaDataGridView(pinguini);
+                    break;
+                case 2:
+                    PopolaDataGridView(rinoceronti);
+                    break;
+                case 3:
+                    PopolaDataGridView(giraffe);
+                    break;
+                case 4:
+                    PopolaDataGridView(scimmie);
+                    break;
+                case 5:
+                    PopolaDataGridView(serpenti);
+                    break;
+            }
+        }
+
+        public class Pinguino
+        {
+            public string Nome { get; set; }
+            public DateTime UltimoPasto { get; set; }
+            public int Età { get; set; }
+            public string Colore { get; set; }
+            public int Peso { get; set; }
+
+            public Pinguino(string nome, DateTime ultimoPasto, int età, string colore, int peso)
+            {
+                Nome = nome;
+                UltimoPasto = ultimoPasto;
+                Età = età;
+                Colore = colore;
+                Peso = peso;
+            }
+        }
+        public class Rinoceronte
+        {
+            string Nome { get; set; }
+            DateTime UltimoPasto { get; set; }
+            int Età { get; set; }
+            int Corni { get; set; }
+            int Peso { get; set; }
+
+            public Rinoceronte(string nome, DateTime ultimoPasto, int età, int corni, int peso)
+            {
+                Nome = nome;
+                UltimoPasto = ultimoPasto;
+                Età = età;
+                Corni = corni;
+                Peso = peso;
+            }
+        }
+        public class Giraffa
+        {
+            string Nome { get; set; }
+            DateTime UltimoPasto { get; set; }
+            int Età { get; set; }
+            int Altezza { get; set; }
+            string Temperamento { get; set; }
+
+            public Giraffa(string nome, DateTime ultimoPasto, int età, int altezza, string temperamento)
+            {
+                Nome = nome;
+                UltimoPasto = ultimoPasto;
+                Età = età;
+                Altezza = altezza;
+                Temperamento = temperamento;
+            }
+        }
+        public class Scimmia
+        {
+            string Nome { get; set; }
+            DateTime UltimoPasto { get; set; }
+            int Età { get; set; }
+            string Temperamento { get; set; }
+            int VelocitàCorsa { get; set; }
+
+            public Scimmia(string nome, DateTime ultimoPasto, int età, string temperamento, int velocitàCorsa)
+            {
+                Nome = nome;
+                UltimoPasto = ultimoPasto;
+                Età = età;
+                Temperamento = temperamento;
+                VelocitàCorsa = velocitàCorsa;
+            }
+        }
+        public class Serpente
+        {
+            string Nome { get; set; }
+            DateTime UltimoPasto { get; set; }
+            int Età { get; set; }
+            int Lunghezza { get; set; }
+            string Colore { get; set; }
+
+            public Serpente(string nome, DateTime ultimoPasto, int età, int lunghezza, string colore)
+            {
+                Nome = nome;
+                UltimoPasto = ultimoPasto;
+                Età = età;
+                Lunghezza = lunghezza;
+                Colore = colore;
+            }
+        }
+    }
+}
